001d7d0d1182f2a60c424bfd60fcb46cedabb367
AileOrgla <aileorgla@gmail.com>
2014-10-20 21:44:33 +0300

    Add files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+   <head>
+    <title>Praktikum3</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type"text/css" href="style.css">
+   </head>
+   <body>
+      <form action="form_processing.php" method="post">
+        <span>Username</span><input type="text" name="username"><br>
+        <span>Password</span><input type="password" name="password"><br>
+        <input type="submit" value="Saada" name="submit"><br>
+      </form>
+
+   </body>
+</html>
